7d78da8d87a02b9c39eea1a7581f87177c74f4e7
Sanjay Patil <sp2004@hotmail.com>
2019-12-12 22:36:19 -0800

    made changes to get random guess on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,28 @@ var songsList = ["AllThat", "InspectorGadget", "FamilyMatters", "FullHouse", "Ru
     "Arthur", "HeyArnold", "DawsonCreek", "DexterLaboratory", "Pokemon", "Friends", "Blossom", "SailorMoon", "AngryBeavers",
     "Doug", "SmartGuy", "TheXFiles"];
 
-function setWord(){
+function setWord() {
     var word = songsList[Math.floor(Math.random() * songsList.length)];
-    var answerArray = [];
-    var answerList=""
-    var newDiv=document.createElement("div")
+    var answerArray = {};
+    //var answerList=""
+    var numgGuess = word.length * 2;
+    var newDiv = ""
+    var list = document.getElementById("guess-word")
+   
     for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
-        answerList=answerList+"  "+answerArray[i]
+        answerArray[i] = word[i];
+        newDiv = newDiv + "<li id=data-pos" + i + " class=letter></li>"
     }
-    
-    newDiv.textContent=answerList
-    newDiv.setAttribute("id","guess-word-string")
-    document.getElementById("guess-word").appendChild(newDiv);
+    //console.log(newDiv)
+    list.innerHTML = newDiv;
+
+    var newDivP = document.createElement("p");
+    newDivP.textContent = numgGuess
+    newDivP.setAttribute("id", "remain-guess");   
+    document.getElementById("number-guess").appendChild(newDivP);
+    //newDiv.textContent=answerList
+    //newDiv.setAttribute("id","guess-word-string")
+    //document.getElementById("guess-word").appendChild(newDiv);
 }
 
 setWord();
