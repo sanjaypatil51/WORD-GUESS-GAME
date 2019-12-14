6a972f1cceedf0e45815d4520f3119dfb4f8f227
Sanjay Patil <sp2004@hotmail.com>
2019-12-13 19:09:41 -0800

    added key event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,22 @@
 
 
 var songsList = ["AllThat", "InspectorGadget", "FamilyMatters", "FullHouse", "Rugrats", "TheMagicSchoolBus", "BoyMeetsWorld",
-    "SavedByTheBell", "Recess", "Animaniacs", "TheFreshPrinceofBel-Air", "SisterSister", "ThePowerpuffGirls",
+    "SavedByTheBell", "Recess", "Animaniacs", "TheFreshPrinceofBelAir", "SisterSister", "ThePowerpuffGirls",
     "Arthur", "HeyArnold", "DawsonCreek", "DexterLaboratory", "Pokemon", "Friends", "Blossom", "SailorMoon", "AngryBeavers",
     "Doug", "SmartGuy", "TheXFiles"];
 
+var answerArray = [];
+var numgGuess = 0;
+var word = ""
+
 function setWord() {
-    var word = songsList[Math.floor(Math.random() * songsList.length)];
-    var answerArray = {};
+    word = songsList[Math.floor(Math.random() * songsList.length)];
+
     //var answerList=""
-    var numgGuess = word.length * 2;
+    numgGuess = word.length * 2;
     var newDiv = ""
     var list = document.getElementById("guess-word")
-   
+
     for (var i = 0; i < word.length; i++) {
         answerArray[i] = word[i];
         newDiv = newDiv + "<li id=data-pos" + i + " class=letter></li>"
@@ -22,13 +26,31 @@ function setWord() {
 
     var newDivP = document.createElement("p");
     newDivP.textContent = numgGuess
-    newDivP.setAttribute("id", "remain-guess");   
+    newDivP.setAttribute("id", "remain-guess");
     document.getElementById("number-guess").appendChild(newDivP);
     //newDiv.textContent=answerList
     //newDiv.setAttribute("id","guess-word-string")
     //document.getElementById("guess-word").appendChild(newDiv);
 }
 
+function checkGuess(){
+
+    
+}
+
 setWord();
+
+document.onkeyup = function (event) {
+    alert(event.which)
+    alert(event.key)
+    var char=event.key
+
+    if (/[a-zA-Z]/i.test(char) && char.length==1){
+        alert("char typed")
+
+
+    }
+    
+}
 
 
